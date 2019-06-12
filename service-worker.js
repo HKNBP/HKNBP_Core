367eb5f1371cb8e59d1a33e53efa7f5ff2ceeeda
iHad169 <27337315+iHad169@users.noreply.github.com>
2019-06-12 21:38:49 +0800

    test

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-//importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+//importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
@@ -13,9 +13,27 @@ if (workbox) {
 var cacheFiles = [
   'index.html'
 ];
+
 workbox.precaching.precacheAndRoute(
     cacheFiles,
     {
         ignoreUrlParametersMatching:[/tvchannel/]
     }
 );
+
+workbox.routing.registerRoute(
+    new RegExp('.*\.*'),
+    workbox.strategies.networkFirst({
+      cacheName: 'cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          // Only cache requests for a week
+          maxAgeSeconds: 1,
+          // Only cache 10 requests.
+          maxEntries: 100,
+        }),
+      ]
+    })
+);
+
+workbox.precaching.cleanupOutdatedCaches();
